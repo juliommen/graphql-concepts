7e2284cd989f061dba88c18ad7ce78113e8af15c
Júlio Mendonça da Costa <juliommen@gmail.com>
2023-06-15 21:01:28 -0300

    final commit

--- server/index.ts
+++ server/index.ts
@@ -2,10 +2,11 @@ import 'reflect-metadata'
 import path from 'path'
 import {buildSchema} from 'type-graphql'
 import { ApolloServer } from 'apollo-server'
+import { UserResolver } from './src/resolvers/UserResolver'
 
 async function main() {
   const schema = await buildSchema({
-    resolvers: [],
+    resolvers: [UserResolver],
     emitSchemaFile: path.resolve(__dirname,'schem.gql'),
   })
 
